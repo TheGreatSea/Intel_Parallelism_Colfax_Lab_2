c98896632b9010a3d1f8ae3cdefe3a1261617047
Rehan Iqbal <rsi7@users.noreply.github.com>
2018-07-21 17:34:24 -0700

    First attempt at parallelization
    Array to store 'n_particles'-size position, random number gen outside
    loops, threshold count check outside loops, loop interchange + SIMD
    declaration

--- diffusion.cc
+++ diffusion.cc
@@ -1,6 +1,9 @@
 #include <mkl.h>
 #include "distribution.h"
 
+/* 
+What I mainly did is same as written: generate random numbers, loop-interchange, forcible vectorization (#pragma omp simd), store particle position, check counts outside nested loops. In my case, vector dependency over nested loops was still remained, so it was partially non-vectorized.
+*/
 
 //vectorize this function based on instruction on the lab page
 int diffusion(const int n_particles, 
@@ -9,17 +12,24 @@ int diffusion(const int n_particles,
               const float alpha, 
               VSLStreamStatePtr rnStream) {
   int n_escaped=0;
-  for (int i = 0; i < n_particles; i++) {
-    float x = 0.0f;
-    for (int j = 0; j < n_steps; j++) {
-      float rn;
+
+       float rn[n_particles];
       
       //Intel MKL function to generate random numbers
-      vsRngUniform(VSL_RNG_METHOD_UNIFORM_STD, rnStream, 1, &rn, -1.0, 1.0);
-      
-      x += dist_func(alpha, rn); 
-    }
+      vsRngUniform(VSL_RNG_METHOD_UNIFORM_STD, rnStream, n_particles, rn, -1.0, 1.0);
+       
+       float x[n_particles];
+       
+       for (int j = 0; j < n_steps; j++) {
+         #pragma omp simd
+           for (int i = 0; i < n_particles; i++) {
+             x[i] = 0.0f;
+             x[i] += dist_func(alpha, rn);
+             } // for {i}
+    
+  } // for {j}
+  for (int k = 0; k < n_particles; k++) {
     if (x > x_threshold) n_escaped++;
-  }
-  return n_escaped;
-}
+    } // for {k}
+    return n_escaped;
+} // diffusion
